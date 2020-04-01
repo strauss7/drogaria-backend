56921669477af321dfd6a5bc95a237e566ac5355
strauss7 <wbsabbyyuji@gmail.com>
2020-04-01 20:11:10 -0300

    Projeto criado

---
+++ src/main/java/com/amira/drogaria1/Drogaria1Application.java
@@ -0,0 +1,13 @@
+package com.amira.drogaria1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Drogaria1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Drogaria1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/amira/drogaria1/Drogaria1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amira.drogaria1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Drogaria1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
